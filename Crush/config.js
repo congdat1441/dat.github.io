65d285e57ae5f328fbf14ccb873c0750ad318212
manhmov3 <99540937+manhmov3@users.noreply.github.com>
2022-02-12 20:18:07 +0700

    Update config.js

--- Crush/config.js
+++ Crush/config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
+    titleWeb: "Gửi pẹn khum quen của tớ",
+    introTitle: 'Pẹn yêu dấu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    desc: 'Thoát web là chấp nhận yêu toy rồi nhó!!!',
+    btnYes: 'Thít lắm <33',
+    btnNo: 'Khum nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao pạn lại yêu mìn <3',
+    btnReply: 'Gửi cho pạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me' //link mess của các bạn. VD: /dinhkaito
+    mess: 'Mìn biết mà 🥰. Yêu pạn nhiều nhiều 😘😘',
+    messDesc: 'Dậy lèm ny mìn nho!!',
+    btnAccept: 'Okiiiii nuôn <3',
+    messLink: 'https://www.facebook.com/messages/t/100023149304811
 }
